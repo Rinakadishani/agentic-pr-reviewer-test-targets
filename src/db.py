e91bb145c56895f8ee5d400eb77809df0361c019
Rina Kadishani <kadishanirina@gmail.com>
2026-03-09 13:44:12 +0100

    test: SQL injection via string concatenation

---
+++ src/db.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+def get_user(username):
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    # Vulnerable: direct string concatenation
+    query = "SELECT * FROM users WHERE username = '" + username + "'"
+    cursor.execute(query)
+    return cursor.fetchone()
+
+def search_products(keyword):
+    conn = sqlite3.connect("store.db")
+    query = "SELECT * FROM products WHERE name LIKE '%" + keyword + "%'"
+    return conn.execute(query).fetchall()
